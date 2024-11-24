48432fae8aa0aab175fec9fa1a0b11ead778e280
MaoDev <bmauricio.cruz07@gmail.com>
2024-11-24 15:50:07 -0500

    Added method getData to map data from API into classes already defined.

--- src/main/java/com/gutendex/gutendex/service/ConvertData.java
+++ src/main/java/com/gutendex/gutendex/service/ConvertData.java
@@ -1,4 +1,19 @@
 package com.gutendex.gutendex.service;
 
-public class ConvertData {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gutendex.gutendex.model.GeneralData;
+
+public class ConvertData implements IConvertData {
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+
+    @Override
+    public <T> T getData(String json, Class<T> classToConvert) {
+        try {
+            return objectMapper.readValue(json, classToConvert);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
